2acd145532734e1d667a50abcf3d5017b7dd7515
Joonas Lähteinen <nemesis.software@protonmail.com>
2020-11-07 17:36:40 +0200

    example

--- src/utils/MathUtils.js
+++ src/utils/MathUtils.js
@@ -1,4 +1,27 @@
 export default class MathUtils {
+    // Example
+    /*
+    import MathUtils from '../../utils/MathUtils'
+
+    const productionTypes = new Set(data.map(farm => farm.water.productionType));
+    for (const type of productionTypes) {
+        const avg_waterUsage = MathUtils.avgWaterUsage(data, type);
+        const avg_PercentageWaterForDrinking = MathUtils.avgWaterUsageForDrinking(data, type);
+        const avg_numberOfanimals = MathUtils.avgNumberOfAnimals(data, type);
+        const avg_co2 = MathUtils.avgCO2(data, type);
+        const avg_CO2PerKg = MathUtils.avgCO2PerKg(data, type);
+        const avg_CO2PerKg = MathUtils.farmCount(data, type);
+
+
+        console.log(`${type}: ${avg_waterUsage} m3/y`);
+        console.log(`${type}: ${avg_PercentageWaterForDrinking} %`);
+        console.log(`${type}: ${avg_numberOfanimals}`);
+        console.log(`${type}: ${avg_co2}`);
+        console.log(`${type}: ${avg_CO2PerKg}`);
+        console.log(`${type}: Farms - ${avg_CO2PerKg}`)
+        console.log("-----------------------");
+    */
+
     static avgWaterUsage (data, type) {
         const farms = data.filter(farm => farm.water.productionType === type);
         const sum = farms.reduce((sum, farm) => sum + farm.water.waterUsage, 0);
