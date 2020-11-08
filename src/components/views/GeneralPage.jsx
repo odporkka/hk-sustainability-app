5d17f25e665fcd90a2b526c21dbe1a2e264f6239
Otto Porkka <ode.porkka@gmail.com>
2020-11-08 02:37:14 +0200

    Demo links added

--- src/components/views/GeneralPage.jsx
+++ src/components/views/GeneralPage.jsx
@@ -4,11 +4,17 @@ import {makeStyles} from "@material-ui/core/styles"
 
 import GeneralHeader from "../content/GeneralHeader"
 import GeneralContent from "../content/GeneralContent"
+import {Typography} from "@material-ui/core"
+import Link from "@material-ui/core/Link"
+import {Link as RouterLink} from "react-router-dom"
 
 // MUI styles
 const useStyles = makeStyles(() => ({
     header: {
         marginTop: 30
+    },
+    link: {
+        color: 'cyan'
     }
 }))
 
@@ -17,6 +23,17 @@ const ProductPage = (props) => {
 
     return (
         <Grid container item xs={12} spacing={2}>
+            <Grid item xs={12} >
+                <Typography variant='h4' component='p' align='center'>
+                    <Link component={RouterLink} to='/1' color='inherit' className={classes.link}>
+                        Link to example of farm view (Demo only)
+                    </Link>
+                </Typography>
+                <Typography variant='body1' align='center' className={classes.paragraph}>
+                    Click above link to view example page to which customer is forwarded from the AR
+                </Typography>
+            </Grid>
+
             <Grid item xs={12} className={classes.header}>
                 <GeneralHeader />
             </Grid>
--- src/components/views/ProductPage.jsx
+++ src/components/views/ProductPage.jsx
@@ -1,15 +1,20 @@
 import React, { useEffect } from 'react'
 import Grid from '@material-ui/core/Grid'
-
+import { Link as RouterLink } from 'react-router-dom'
+import Link from '@material-ui/core/Link'
 import ProductHeader from "../content/ProductHeader"
 import ProductChainChart from "../content/ProductChainChart"
 import {makeStyles} from "@material-ui/core/styles"
+import {Typography} from "@material-ui/core"
 
 
 // MUI styles
 const useStyles = makeStyles(() => ({
     header: {
         marginTop: 30
+    },
+    link: {
+        color: 'cyan'
     }
 }))
 
@@ -31,6 +36,13 @@ const ProductPage = (props) => {
                 <ProductChainChart id={id} />
             </Grid>
 
+            <Grid item xs={12} >
+                <Typography variant='h3' component='p' align='center'>
+                    <Link component={RouterLink} to='/' color='inherit' className={classes.link}>
+                        Learn more about HKScan
+                    </Link>
+                </Typography>
+            </Grid>
         </Grid>
     )
 }
